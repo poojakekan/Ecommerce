e25c6a061e69948fbfc1cc87ed19ca9884b0bf44
Pooja Kekan <35685820+poojakekan@users.noreply.github.com>
2024-07-10 17:43:32 +0530

    thiird commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+      extend: {
+        fontFamily: {
+          "metal-mania": ["MetalMania", "sans-serif"],
+        },
+      },
+    },
+    plugins: [],
+  };
